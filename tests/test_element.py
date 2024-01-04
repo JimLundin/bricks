4e3a0f87d723b3a707609f17782ea57621af33ef
JimLundin <jimeriklundin@gmail.com>
2024-01-04 14:40:54 +0100

    feat: tests for nested elements

--- tests/test_element.py
+++ tests/test_element.py
@@ -59,3 +59,57 @@ def test_multiple_attributes_and_content_with_formatting() -> None:
 def test_attribute_with_underscore() -> None:
     element = Element("input")(type_="text", placeholder="Enter text")
     assert str(element) == '<input type="text" placeholder="Enter text">'
+
+
+def test_nested_elements_with_strings() -> None:
+    element = Element("div")(
+        Element("p")("Paragraph 1"),
+        Element("span", class_="highlight")("Nested Span"),
+        '<a href="https://example.com">Link</a>',
+        Element("div")(Element("strong")("Nested Strong")),
+    )
+    expected_result = (
+        "<div>"
+        "<p>Paragraph 1</p>"
+        '<span class="highlight">Nested Span</span>'
+        '<a href="https://example.com">Link</a>'
+        "<div><strong>Nested Strong</strong></div>"
+        "</div>"
+    )
+    assert str(element) == expected_result
+
+
+def test_nested_elements_with_f_string() -> None:
+    element = Element("div")(
+        "<p>Paragraph 1</p>",
+        Element("span")("Nested Span"),
+        '<a href="https://example.com">Link</a>',
+        "<div><strong>Nested Strong</strong></div>",
+    )
+    expected_result = (
+        "<div>"
+        "<p>Paragraph 1</p>"
+        "<span>Nested Span</span>"
+        '<a href="https://example.com">Link</a>'
+        "<div><strong>Nested Strong</strong></div>"
+        "</div>"
+    )
+    assert str(element) == expected_result
+
+
+def test_nested_elements_with_f_strings() -> None:
+    element = Element("div")(
+        "<p>Paragraph 1</p>",
+        f'Embedded Element: {Element("span")("Nested Span")}',
+        '<a href="https://example.com">Link</a>',
+        "<div><strong>Nested Strong</strong></div>",
+    )
+    expected_result = (
+        "<div>"
+        "<p>Paragraph 1</p>"
+        "Embedded Element: <span>Nested Span</span>"
+        '<a href="https://example.com">Link</a>'
+        "<div><strong>Nested Strong</strong></div>"
+        "</div>"
+    )
+    assert str(element) == expected_result
